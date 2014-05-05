a814d95fe66b3f47feb4d50f46b4794949234c02
Dongliang Yu <yudl.nju@gmail.com>
2014-05-05 20:09:13 +0800

    update sqrt with long to replave int overflow check

--- src/solution/Sqrt.java
+++ src/solution/Sqrt.java
@@ -11,29 +11,28 @@ public class Sqrt {
     public static int sqrt(int x) {
         if (x <= 0)
             return 0;
-        int max_sqrt = (int) Math.pow(2, 16);
-        return range(1, Math.max(x, max_sqrt), x);
+        return range(1, x, x);
     }
     
     private static int range(int lo, int hi, int x) {
         if (lo + 1 == hi)
             return lo;
         int mid = lo + (hi - lo) / 2;
-        int mul = mid * mid;
+        long mul = (long) mid * (long) mid;
         if (mul == x)
             return mid;
-        else if (overflow(mid, mid) || mul > x)
+        else if (mul > x)
             return range(lo, mid, x);
         else
             return range(mid, hi, x);
     }
-    
+    /*
     private static boolean overflow(int a, int b) {
         if (a != 0 && Integer.MAX_VALUE/a < b)
             return true;
         else
             return false;
-    }
+    }*/
 
     public static void main(String[] args) {
         System.out.println(sqrt(0));
