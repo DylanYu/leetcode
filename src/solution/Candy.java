d50400cb5ad2680c3c29d01d79cf75712ba2a5db
Dongliang Yu <yudl.nju@gmail.com>
2014-05-13 12:28:32 +0800

    candy half done

--- src/solution/Candy.java
+++ src/solution/Candy.java
@@ -1,5 +1,7 @@
 package solution;
 
+import java.util.ArrayList;
+
 /**
  * There are N children standing in a line. Each child is assigned a rating value.
  * <p>
@@ -15,33 +17,87 @@ package solution;
  *
  */
 public class Candy {
-    public static int candy_bf(int[] ratings) {
-        int len = ratings.length;
-        int[] candy = new int[len];
-        for (int i = 0; i < len; i++)
-            candy[i] = 1;
-        boolean change = true;
-        while (change) {
-            change = false;
-            for (int i = 0; i < len; i++) {
-                if (i - 1 >= 0 && ratings[i-1] < ratings[i] && candy[i-1] >= candy[i]) {
-                    candy[i] = candy[i-1] + 1;
-                    change = true;
-                }
-                if (i + 1 < len && ratings[i+1] < ratings[i] && candy[i+1] >= candy[i]) {
-                    candy[i] = candy[i+1] + 1;
-                    change = true;
-                }
+    static class Node {
+        int rating;
+        int candy;
+        Node left;
+        int l; // -1, 0, 1 : less, equal, greater
+        Node right;
+        int r; // -1, 0, 1 : less, equal, greater
+        public Node(int r) {
+            rating = r;
+            candy = 1;
+            left = null;
+            l = -1;
+            right = null;
+            r = -1;
+        }
+    }
+    
+    public static int candy(int[] ratings) {
+        Node dummyhd = new Node(Integer.MAX_VALUE);
+        Node walker = dummyhd;
+        for (int e : ratings) {
+            Node node = new Node(e);
+            walker.right = node;
+            node.left = walker;
+            if (walker.rating > node.rating) {
+                walker.r = 1;
+                node.l = -1;
+            } else if (walker.rating < node.rating) {
+                walker.r = -1;
+                node.l = 1;
+            } else {
+                walker.r = 0;
+                node.l = 0;
+            }
+            walker = node;
+        }
+        // find all valleys
+        ArrayList<Node> valleys = new ArrayList<Node>();
+        walker = dummyhd.right;
+        while (walker != null) {
+            if (walker.l == -1 && walker.r == -1 ||
+                    walker.l == 0 && walker.r == -1)
+                // TODO
+                valleys.add(walker);
+            walker = walker.right;
+        }
+        // for each valley, scan its left and right part
+        for (Node v : valleys) {
+            walker = v.left;
+            while (walker != dummyhd) {
+                // TODO
+                if (walker.r == 1)
+                    walker.candy = Math.max(walker.candy, walker.right.candy + 1);
+                else if (walker.r == 0)
+                    walker.candy = walker.right.candy;
+                else // break at turn down point
+                    break;
+                walker = walker.left;
+            }
+            walker = v.right;
+            while (walker != null) {
+                // TODO
+                if (walker.l == 1)
+                    walker.candy = Math.max(walker.candy, walker.left.candy + 1);
+                else // break at turn point and equal case (no need to deal with equal cases twice)
+                    break;
+                walker = walker.right;
             }
         }
+        // collect result
+        walker = dummyhd.right;
         int number = 0;
-        for (int e : candy)
-            number += e;
+        while (walker != null) {
+            number += walker.candy;
+            walker = walker.right;
+        }
         return number;
     }
     
     public static void main(String[] args) {
         int[] ratings = {1, 5, 4, 3, 7, 2, 6};
-        System.out.println(candy_bf(ratings));
+        System.out.println(candy(ratings));
     }
 }
