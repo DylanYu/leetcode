6ca8092a980b1123f276ba298d758baa2a9696d1
Dongliang Yu <yudl.nju@gmail.com>
2014-05-19 09:53:39 +0800

    fix doc

--- src/solution/AddTwoNumbers.java
+++ src/solution/AddTwoNumbers.java
@@ -4,9 +4,9 @@ package solution;
  * You are given two linked lists representing two non-negative numbers. The digits are stored in 
  * reverse order and each of their nodes contain a single digit. Add the two numbers and return 
  * it as a linked list.
-
-Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-Output: 7 -> 0 -> 8
+ * <p>
+ * <p>Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * <p>Output: 7 -> 0 -> 8
  * 
  * @author Dongliang Yu
  *
