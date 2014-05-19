fc2ef7c60392a6537b7f5f53922ff7f93a9bf790
Dongliang Yu <yudl.nju@gmail.com>
2014-05-19 22:38:20 +0800

    single number with an xor awesome solution

---
+++ src/solution/SingleNumber.java
@@ -0,0 +1,36 @@
+package solution;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+/**
+ * Given an array of integers, every element appears twice except for one. Find that single one.
+ * <p>
+ * <p>Note:
+ * <p>Your algorithm should have a linear runtime complexity. Could you implement it without 
+ * using extra memory?
+ *
+ * @author Dongliang Yu
+ *
+ */
+public class SingleNumber {
+    /**
+     * Awesome solution 
+     */
+    public static int singleNumber(int[] A) {
+        int single = 0;
+        for(int e : A)
+            single ^= e;
+        return single;
+    }
+    
+    public static int singleNumberWithExtraSpace(int[] A) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int e : A) {
+            if (set.contains(e)) set.remove(e);
+            else set.add(e);
+        }
+        Iterator<Integer> it = set.iterator();
+        return it.next();
+    }
+}
