8772811c6167c41d2a11bdd82fe3004d64bc1942
Dongliang Yu <yudl.nju@gmail.com>
2014-05-19 12:21:08 +0800

    make it nlogn

--- src/solution/SearchInRotatedSortedArray.java
+++ src/solution/SearchInRotatedSortedArray.java
@@ -1,35 +1,59 @@
 package solution;
 
+/**
+ * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+ * <p>
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ * <p>
+ * You are given a target value to search. If found in the array return its index, 
+ * otherwise return -1.
+ * <p>
+ * You may assume no duplicate exists in the array.
+ * 
+ * @author Dongliang Yu
+ *
+ */
 public class SearchInRotatedSortedArray {
-    private static int len;
-    private static int shift;
-
+    private static boolean flag;
+    
     public static int search(int[] A, int target) {
-        len = A.length;
-        shift = 0;
-        for (int i = 1; i < len; i++) {
-            if (A[i] < A[i-1]) {
-                shift = i;
-                break;
-            }
-        }
-        return binarySearch(A, 0, len - 1, target);
+        flag = false;
+        return binary(A, 0, A.length - 1, target);
     }
     
-    private static int binarySearch(int[] A, int lo, int hi, int target) {
+    private static int binary(int[] A, int lo, int hi, int target) {
         if (lo > hi) return -1;
         int mid = lo + (hi - lo) / 2;
-        if (A[convert(mid)] == target) return convert(mid);
-        else if (A[convert(mid)] < target) return binarySearch(A, mid + 1, hi, target);
-        else return binarySearch(A, lo, mid - 1, target);
-    }
-    
-    private static int convert(int index) {
-        return (index + shift) % len;
-    }
-
-    public static void main(String[] args) {
-        int[] A = {4, 5, 6, 7, 0, 1, 2};
-        System.out.println(search(A, 1));
+        int cur = A[mid];
+        if (cur == target) {
+            return mid;
+        } else if(flag) {
+            if (cur < target) return binary(A, mid + 1, hi, target);
+            else return binary(A, lo, mid - 1, target);
+        } else {
+            if (cur < A[hi]) {
+                if (cur < target) {
+                    int left = binary(A, lo, mid - 1, target);
+                    if (left != -1) return left;
+                    else {
+                        flag = true;
+                        return binary(A, mid + 1, hi, target);
+                    }
+                } else {
+                    return binary(A, lo, mid - 1, target);
+                }
+            } else {
+                if (cur < target) {
+                    return binary(A, mid + 1, hi, target);
+                } else {
+                    int right = binary(A, mid + 1, hi, target);
+                    if (right != -1) return right;
+                    else {
+                        flag = true;
+                        return binary(A, lo, mid - 1, target);
+                    }
+                }
+            }
+        }
     }
 }
