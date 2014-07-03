16c31a70ef16218f36d84b1b98fe0900f820c3d1
Dongliang Yu <yudl.nju@gmail.com>
2014-07-04 07:22:25 +0800

    word break II title

--- src/solution/WordBreakII.java
+++ src/solution/WordBreakII.java
@@ -5,6 +5,14 @@ import java.util.List;
 import java.util.Set;
 
 /**
+ * Given a string s and a dictionary of words dict, add spaces in s to construct a sentence where 
+ * each word is a valid dictionary word. Return all such possible sentences.
+ * <p>
+ * For example, given
+ * s = "catsanddog",
+ * dict = ["cat", "cats", "and", "sand", "dog"].
+ *
+ * A solution is ["cats and dog", "cat sand dog"].
  * 
  * @author Dongliang Yu
  *
