9edc00e6cb8438f21c59ea417629ca1b7a899ed6
Dongliang Yu <yudl.nju@gmail.com>
2014-07-28 23:07:19 +0800

    convert sorted list to BST using extra array

---
+++ src/solution/ConvertSortedListToBST.java
@@ -0,0 +1,32 @@
+package solution;
+
+/**
+ * Given a singly linked list where elements are sorted in ascending order, 
+ * convert it to a height balanced BST.
+ * 
+ * @author Dongliang Yu
+ *
+ */
+public class ConvertSortedListToBST {
+    public TreeNode sortedListToBST(ListNode head) {
+        int n = 0;
+        for (ListNode walker = head; walker != null; walker = walker.next) n++;
+        int[] A = new int[n];
+        ListNode walker = head;
+        for(int i = 0; i < n; i++) {
+            A[i] = walker.val;
+            walker = walker.next;
+        }
+        return sortedArrayToBST(A, 0, n-1);
+    }
+    
+    // [lo, hi]
+    private TreeNode sortedArrayToBST(int[] A, int lo, int hi) {
+        if (lo > hi) return null;
+        int mid = lo + (hi - lo) / 2;
+        TreeNode root = new TreeNode(A[mid]);
+        root.left = sortedArrayToBST(A, lo, mid-1);
+        root.right = sortedArrayToBST(A, mid+1, hi);
+        return root;
+    }
+}
