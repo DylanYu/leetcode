9556a2c3040c108e0931a80869db409dc7b8b141
Dongliang Yu <yudl.nju@gmail.com>
2014-07-30 08:28:11 +0800

    BinaryTreeLevelOrderTraversal add recursive way

--- src/solution/BinaryTreeLevelOrderTraversal.java
+++ src/solution/BinaryTreeLevelOrderTraversal.java
@@ -1,7 +1,9 @@
 package solution;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Given a binary tree, return the level order traversal of its nodes' values. 
@@ -25,14 +27,51 @@ import java.util.List;
  *
  */
 public class BinaryTreeLevelOrderTraversal {
+    // interesting recursive solution
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> rst = new ArrayList<List<Integer>>();
-        List<TreeNode> level = new ArrayList<TreeNode>();
+        levelOrder(root, 0, rst);
+        return rst;
+    }
+    
+    private void levelOrder(TreeNode node, int level, List<List<Integer>> rst) {
+        if (node == null) return;
+        if (rst.size() == level) rst.add(new LinkedList<Integer>());
+        rst.get(level).add(node.val);
+        levelOrder(node.left, level+1, rst);
+        levelOrder(node.right, level+1, rst);
+    }
+    
+    /*
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> rst = new ArrayList<List<Integer>>();
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        if (root != null) queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode end = queue.getLast();
+            List<Integer> levelRst = new LinkedList<Integer>();
+            while (true) {
+                TreeNode got = queue.removeFirst();
+                levelRst.add(got.val);
+                if (got.left != null) queue.add(got.left);
+                if (got.right != null) queue.add(got.right);
+                if (got == end) break;
+            }
+            rst.add(levelRst);
+        }
+        return rst;
+    }
+    */
+    
+    /* another similar solution
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> rst = new ArrayList<List<Integer>>();
+        List<TreeNode> level = new LinkedList<TreeNode>();
         if (root != null) level.add(root);
         while (level.size() > 0) {
             List<Integer> levelRst = new ArrayList<Integer>();
-            List<TreeNode> nextLevel = new ArrayList<TreeNode>();
-            for(TreeNode node : level) {
+            List<TreeNode> nextLevel = new LinkedList<TreeNode>();
+            for (TreeNode node : level) {
                 levelRst.add(node.val);
                 if (node.left != null) nextLevel.add(node.left);
                 if (node.right != null) nextLevel.add(node.right);
@@ -42,4 +81,5 @@ public class BinaryTreeLevelOrderTraversal {
         }
         return rst;
     }
+    */
 }
