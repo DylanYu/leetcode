d849dde8b65647ed8f93a0573b1fe6bbb6ab12fd
Dongliang Yu <yudl.nju@gmail.com>
2014-08-08 20:46:26 +0800

    generate parentheses

---
+++ src/solution/GenerateParentheses.java
@@ -0,0 +1,42 @@
+package solution;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class GenerateParentheses {
+    class Node {
+        int l;
+         int r;
+         String s;
+         Node (int l, int r, String s) {
+             this.l = l;
+             this.r = r;
+             this.s = s;
+         }
+     }
+     
+     public List<String> generateParenthesis(int n) {
+         List<String> rst = new LinkedList<String>();
+         //if (n == 0) return rst; // later logic can handle this case
+         LinkedList<Node> queue = new LinkedList<Node>();
+         Node node = new Node(1, 0, "(");
+         queue.add(node);
+         while (queue.size() > 0) {
+             Node cur = queue.removeFirst();
+             if (cur.l + cur.r > n*2) continue;
+             if (cur.l == cur.r && cur.l + cur.r == n*2) {
+                 rst.add(cur.s);
+                 continue;
+             }
+             if (cur.l < cur.r) continue;
+             if (cur.l > cur.r) { // if l == r, add ")" will get an invalid case
+                 Node plusRight = new Node(cur.l, cur.r + 1, cur.s+")");
+                 queue.add(plusRight);
+             }
+             cur.l++;
+             cur.s = cur.s + "(";
+             queue.add(cur);
+         }
+         return rst;
+     }
+}
