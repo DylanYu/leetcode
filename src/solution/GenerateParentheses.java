04eef62c7291de41e21a2a90c79dc4075e041e00
Dongliang Yu <yudl.nju@gmail.com>
2014-08-08 21:39:17 +0800

    add doc

--- src/solution/GenerateParentheses.java
+++ src/solution/GenerateParentheses.java
@@ -3,6 +3,16 @@ package solution;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ *
+ * For example, given n = 3, a solution set is:
+ *
+ * "((()))", "(()())", "(())()", "()(())", "()()()"
+ * 
+ * @author Dongliang Yu
+ *
+ */
 public class GenerateParentheses {
     class Node {
         int l;
