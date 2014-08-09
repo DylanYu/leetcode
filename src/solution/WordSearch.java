bf897eaeca1b4cafdd2e3f5a952760b903c78c4a
Dongliang Yu <yudl.nju@gmail.com>
2014-08-09 15:58:58 +0800

    word search update

--- src/solution/WordSearch.java
+++ src/solution/WordSearch.java
@@ -1,5 +1,8 @@
 package solution;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Given a 2D board and a word, find if the word exists in the grid.
  *
@@ -58,6 +61,69 @@ public class WordSearch {
             return false;
         }
     }
+    
+    /*
+     * Interesting solution uses an extra class Position to store visited path, almost same as 
+     * the above one and not necessary to apply it in this problem but worth understanding the 
+     * practice of overriding equals and hashCode.
+     *  
+    class Position {
+        int x;
+        int y;
+        public Position(int x, int y) { this.x = x; this.y = y; }
+        
+        @Override 
+        public boolean equals(Object obj) {
+            if (obj == this) return true;
+            if (!(obj instanceof Position)) return false;
+            Position that = (Position) obj;
+            return this.x == that.x && this.y == that.y;
+        }
+        
+        @Override
+        public int hashCode() {
+            int result = 17;
+            result = result * 31 + x;
+            result = result * 31 + y;
+            return result;
+        }
+    }
+    
+    public boolean exist(char[][] board, String word) {
+        if (board == null) return false;
+        if (word == null || word.length() == 0) return false;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (exist(board, i, j, word, 0, new HashSet<Position>()))
+                    return true;
+            }
+        }
+        return false;
+    }
+    
+    // DFS
+    private boolean exist(char[][] board, int i, int j, String word, int index, Set<Position> set) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length)
+            return false;
+        if (index >= word.length()) return false;
+        if (set.contains(new Position(i, j))) return false;
+        char curBoard = board[i][j];
+        char curWord = word.charAt(index);
+        if (curBoard != curWord) return false;
+        if (index == word.length()-1) return true;
+        set.add(new Position(i, j));
+        boolean exist = exist(board, i-1, j, word, index+1, set)
+                        || exist(board, i+1, j, word, index+1, set)
+                        || exist(board, i, j-1, word, index+1, set)
+                        || exist(board, i, j+1, word, index+1, set);
+        if (exist) return true;
+        else {
+            // go back to try another way and remove current position in set
+            set.remove(new Position(i, j));
+            return false;
+        }
+    }
+    */
     
     public static void main(String[] args) {
         char[][] board = {"ABCE".toCharArray(), "SFCS".toCharArray(), "ADEE".toCharArray()};
