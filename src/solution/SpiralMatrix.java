61c56895a19df91e968b309cf1a286f1131f394e
Dongliang Yu <yudl.nju@gmail.com>
2014-08-28 23:06:38 +0800

    add doc

--- src/solution/SpiralMatrix.java
+++ src/solution/SpiralMatrix.java
@@ -3,6 +3,22 @@ package solution;
 import java.util.List;
 import java.util.LinkedList;
 
+/**
+ * Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+ * 
+ * For example,
+ * Given the following matrix:
+ * 
+ * [
+ *  [ 1, 2, 3 ],
+ *  [ 4, 5, 6 ],
+ *  [ 7, 8, 9 ]
+ * ]
+ * You should return [1,2,3,6,9,8,7,4,5].
+ * 
+ * @author helo
+ *
+ */
 public class SpiralMatrix {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> list = new LinkedList<Integer>();
