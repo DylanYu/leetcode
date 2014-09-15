e511458f25eba31725a78c6d064030cef10844bc
Dongliang Yu <yudl.nju@gmail.com>
2014-09-15 09:15:18 +0800

    straight forward solution

--- src/solution/PartitionList.java
+++ src/solution/PartitionList.java
@@ -1,7 +1,20 @@
 package solution;
 
+/**
+ * Given a linked list and a value x, partition it such that all nodes less than x come before 
+ * nodes greater than or equal to x.
+ * 
+ * You should preserve the original relative order of the nodes in each of the two partitions.
+ * 
+ * For example,
+ * Given 1->4->3->2->5->2 and x = 3,
+ * return 1->2->2->4->3->5.
+ * 
+ * @author Dongliang Yu
+ *
+ */
 public class PartitionList {
-    
+    // one pass solution
     public ListNode partition(ListNode head, int x) {
         if (head == null)
             return null;
@@ -47,6 +60,37 @@ public class PartitionList {
         less.next = greaterHead;
         return dummyNode.next;
     }
+    
+    /*
+    public ListNode partition(ListNode head, int x) {
+        if (head == null) return null;
+        //if (head.next == null) return head;
+        ListNode p = head;
+        int L = 1;
+        while (p.next != null) {
+            p = p.next;
+            L++;
+        }
+        ListNode tail = p;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode p1 = dummy;
+        ListNode p2 = head;
+        for (int i = 0; i < L; i++) {
+            if (p2.val >= x && p2 != tail) { // p2 != tail is tricky case
+                p1.next = p2.next;
+                tail.next = p2;
+                tail = p2;
+                tail.next = null;
+                p2 = p1.next;
+            } else {
+                p1 = p2;
+                p2 = p2.next;
+            }
+        }
+        return dummy.next;
+    }
+    */
     
     public static void main(String[] args) {
 //        ListNode head = LinkedList.createCertainList(new int[]{60,52,65,68,26,93,85,69,62,4});
