6dce8d4aa3aaffb2d6bca0ba718c5e60e6e5dc81
Dongliang Yu <yudl.nju@gmail.com>
2014-09-26 20:02:20 +0800

    revise

--- src/solution/Atoi.java
+++ src/solution/Atoi.java
@@ -7,38 +7,40 @@ package solution;
  *
  */
 public class Atoi {
+	private static final int MAX = Integer.MAX_VALUE;
+    
+    // +, -, overflow , 0000000123, if encounter invalid character return last result
     public static int atoi(String str) {
+        if (str == null) return 0; // error
         str = str.trim();
-        if (str.equals("")) return 0;
-        char[] a = str.toCharArray();
-        int length = a.length;
-        int num = 0;
-        boolean positive = true;
+        char[] c = str.toCharArray();
+        int len = c.length;
+        if (len == 0) return 0; // error
+        int sign = 0;
         int i = 0;
-        if (a[i] == '-') {
-            positive = false;
-            i++;
-        } else if (a[i] == '+') {
-            positive = true;
-            i++;
+        if (c[0] == '+') { sign = 1; i++; }
+        else if (c[0] == '-') { sign = -1; i++; }
+        else sign = 1;
+        int num = 0;
+        while (i < len) {
+            char ch = c[i];
+            if (!(ch >= '0' && ch <= '9')) break; // error
+            int digit = ch - '0';
+            // deal with overflow
+            if (sign == 1) {
+                if (num > MAX/10) return MAX;
+                num *= 10;
+                if (num > MAX-digit) return MAX;
+                num += digit;
+            } else {
+                if (num > ((long) MAX + 1) / 10) return -MAX-1;
+                num *= 10;
+                if (num > (long) MAX + 1 - digit) return -MAX-1;
+                num += digit;
+            }
+            i++; //
         }
-        while (i < length) {
-            char c = a[i];
-            if (c >= '0' && c <= '9') {
-                int digit = c - '0';
-                long shifted = (long) num * 10; // overflow is possible here
-                long diff = Integer.MAX_VALUE - shifted;
-                if (positive) {
-                    if (diff <= digit) return Integer.MAX_VALUE;
-                } else {
-                    if (diff <= digit - 1) return Integer.MIN_VALUE;
-                }
-                num = (int) shifted + digit; // shifted is not overflowed thanks for previous check
-            } else
-                break;
-            i++;
-        }
-        return positive ? num : -num;
+        return sign * num;
     }
 
     public static void main(String[] args) {
