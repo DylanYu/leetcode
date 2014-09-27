d2bf9597f4cc3ee782d0000716cf700a0887526d
Dongliang Yu <yudl.nju@gmail.com>
2014-09-27 10:50:47 +0800

    neater

--- src/solution/SameTree.java
+++ src/solution/SameTree.java
@@ -11,9 +11,7 @@ package solution;
  */
 public class SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true;
-        else if (p == null && q != null || p != null && q == null) return false;
-        else if (p.val != q.val) return false;
-        else return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        if (p == null || q == null) return p == null && q == null;
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
