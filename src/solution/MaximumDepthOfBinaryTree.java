912380d8d420ec9ee8cea2889ffc50002535c82e
Dongliang Yu <yudl.nju@gmail.com>
2014-09-27 10:46:56 +0800

    another kind of recursion

--- src/solution/MaximumDepthOfBinaryTree.java
+++ src/solution/MaximumDepthOfBinaryTree.java
@@ -14,4 +14,15 @@ public class MaximumDepthOfBinaryTree {
         if (root == null) return 0;
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
+    
+    /*
+    public int maxDepth(TreeNode root) {
+        return maxDepth(root, 0);
+    }
+    
+    private int maxDepth(TreeNode node, int height) {
+        if (node == null) return height;
+        return Math.max(maxDepth(node.left, height+1), maxDepth(node.right, height+1));
+    }
+    */
 }
