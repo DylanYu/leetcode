add95dd9f6314a8af1c0c10dc97b68f7cccbf1b1
Dongliang Yu <yudl.nju@gmail.com>
2014-09-27 09:51:23 +0800

    alternative solution

--- src/solution/RomanToInteger.java
+++ src/solution/RomanToInteger.java
@@ -28,4 +28,31 @@ public class RomanToInteger {
         }
         return number;
     }
+    
+    /*
+    private int getInt(char c) {
+        switch (c) {
+            case 'I': return 1; // no statement after return, or error when compiling
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return -1;
+        }
+    }
+    
+    public int romanToInt(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int num = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i == s.length()-1 || i < s.length()-1 && getInt(s.charAt(i)) >= getInt(s.charAt(i+1)))
+                num += getInt(s.charAt(i));
+            else
+                num -= getInt(s.charAt(i));
+        }
+        return num;
+    }
+    */
 }
