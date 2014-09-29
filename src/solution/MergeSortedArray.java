721746b965b47d8a9e482f008d3639ff7a228c8d
Dongliang Yu <yudl.nju@gmail.com>
2014-09-29 17:08:34 +0800

    merge from large to small

--- src/solution/MergeSortedArray.java
+++ src/solution/MergeSortedArray.java
@@ -11,6 +11,22 @@ package solution;
  *
  */
 public class MergeSortedArray {
+    // merge from large to small to eliminate copying
+    public void merge(int A[], int m, int B[], int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while (i >= 0 && j >= 0) {
+            if (A[i] > B[j]) A[k--] = A[i--];
+            else A[k--] = B[j--];
+        }
+        if (i == -1)
+            while (j >= 0) A[k--] = B[j--];
+        else
+            return;
+    }
+    
+    /*
     public void merge(int A[], int m, int B[], int n) {
         if (n <= 0)
             return;
@@ -39,4 +55,5 @@ public class MergeSortedArray {
         else // after walking through B, it is already merged 
             return;
     }
+    */
 }
