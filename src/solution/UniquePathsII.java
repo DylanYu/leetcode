b9a19395a4c78fba4921a60681c4605259d899d0
Dongliang Yu <yudl.nju@gmail.com>
2014-09-29 20:58:58 +0800

    more approaches

--- src/solution/UniquePathsII.java
+++ src/solution/UniquePathsII.java
@@ -43,6 +43,37 @@ public class UniquePathsII {
     }
     
     /*
+     * O(n) space DP
+     * 
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        if (m == 0) return 0;
+        int n = obstacleGrid[0].length;
+        if (n == 0) return 0;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
+        int[] A = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (obstacleGrid[0][i] == 0) A[i] = 1;
+            else
+                while (i < n) A[i++] = 0;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) { // have to start from 0 to detect obstacle
+                if (obstacleGrid[i][j] == 1) A[j] = 0;
+                else {
+                    if (j == 0) {
+                        if (A[j] == 0) A[j] = 0; //
+                        else A[j] = 1;
+                    } else
+                        A[j] = A[j] + A[j-1];
+                }
+            }
+        }
+        return A[n-1];
+    }
+    */
+    
+    /*
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
