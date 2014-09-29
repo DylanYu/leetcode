ba6a54987e48ed0eb16c24fe9afa4c4fb65f9a4c
Dongliang Yu <yudl.nju@gmail.com>
2014-09-29 23:31:19 +0800

    revise

--- src/solution/PascalTriangleII.java
+++ src/solution/PascalTriangleII.java
@@ -14,11 +14,12 @@ import java.util.List;
  */
 public class PascalTriangleII {
     public List<Integer> getRow(int rowIndex) {
+        //if (rowIndex < 0) return new ArrayList<Integer>();
         ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(1);
-        for (int i = 0; i < rowIndex; i++) {
-            for (int j = list.size()-1; j >= 1; j--)
-                list.set(j, list.get(j) + list.get(j-1));
+        //list.add(1);
+        for (int i = 0; i <= rowIndex; i++) {
+            for (int j = list.size()-1; j >= 1; j--) // set from tail to head to avoid poisoning data
+                list.set(j, list.get(j)+list.get(j-1)); // set not add
             list.add(1);
         }
         return list;
