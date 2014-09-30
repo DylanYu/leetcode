5a59a20713f0e49b65f616c1badcc41084c29ac2
Dongliang Yu <yudl.nju@gmail.com>
2014-10-01 03:36:37 +0800

    add a new approach

--- src/solution/PathSum.java
+++ src/solution/PathSum.java
@@ -1,5 +1,8 @@
 package solution;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding 
  * up all the values along the path equals the given sum.
@@ -19,6 +22,7 @@ package solution;
  *
  */
 public class PathSum {
+	// decided by leaf
     public static boolean hasPathSum(TreeNode root, int sum) {
         return subroutine(root, 0, sum);
     }
@@ -29,4 +33,28 @@ public class PathSum {
         if (now == sum && node.left == null && node.right == null) return true;
         return subroutine(node.left, now, sum) || subroutine(node.right, now, sum);
     }
+    
+    /**
+     * Another recursive approach, decided by root
+     * 
+    public boolean hasPathSum(TreeNode root, int sum) {
+        //if (root == null) return false;
+        Set<Integer> sums = pathSum(root);
+        for (int e : sums)
+            if (e == sum) return true;
+        return false;
+    }
+    
+    private Set<Integer> pathSum(TreeNode node) {
+        Set<Integer> ret = new HashSet<Integer>();
+        if (node == null) return ret;
+        
+        Set<Integer> lefts = pathSum(node.left);
+        for (int l : lefts) ret.add(l+node.val);
+        Set<Integer> rights = pathSum(node.right);
+        for (int r : rights) ret.add(r+node.val);
+        if (ret.size() == 0) ret.add(node.val);
+        return ret;
+    }
+    */
 }
