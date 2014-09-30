2cc0a54d322a4ba9f15a9749a514ea87f7efe008
Dongliang Yu <yudl.nju@gmail.com>
2014-09-30 21:23:20 +0800

    more neat

--- src/solution/SpiralMatrix.java
+++ src/solution/SpiralMatrix.java
@@ -21,37 +21,42 @@ import java.util.LinkedList;
  */
 public class SpiralMatrix {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> list = new LinkedList<Integer>();
-        if (matrix == null) return list;
+        List<Integer> ret = new LinkedList<Integer>();
+        if (matrix == null) return ret;
         int m = matrix.length;
-        if (m == 0) return list; // keep attention..
+        if (m == 0) return ret; // keep attention..
         int n = matrix[0].length;
+        //if (n == 0) return ret;
         int x0 = 0; // most left up point
         int y0 = 0;
         int x1 = m-1; // most right bottom point
         int y1 = n-1;
-        int x = 0, y = 0;
-        while(x0 < x1 && y0 < y1) {
-            x = x0; // after one loop, (x, y) goes back to original position, must set them 'forward'
-            y = y0;
+        //while(x0 < x1 && y0 < y1) {
+        while(x0 <= x1 && y0 <= y1) {
+            int x = x0; // after one loop, (x, y) goes back to original position, must set them 'forward'
+            int y = y0;
             // traverse around
-            while (y < y1) list.add(matrix[x][y++]);
-            while (x < x1) list.add(matrix[x++][y]);
-            while (y > y0) list.add(matrix[x][y--]);
-            while (x > x0) list.add(matrix[x--][y]);
+            while (y < y1) ret.add(matrix[x][y++]);
+            while (x < x1) ret.add(matrix[x++][y]);
+            // one row or col or one block left case
+            if (x == x0 || y == y0) { ret.add(matrix[x][y]); break; }
+            while (y > y0) ret.add(matrix[x][y--]);
+            while (x > x0) ret.add(matrix[x--][y]);
             x0++;
             y0++;
             x1--;
             y1--;
         }
+        /*
         x = x0; // set 'forward'
         y = y0;
-        if (x0 == x1 && x0 <= y1) {
+        if (x0 == x1 && y0 <= y1) {
             while (y <= y1) list.add(matrix[x][y++]);
         } else if (y0 == y1 && x0 <= x1) { // use 'else' to keep away from 'counting twice when x0==x1 && y0==y1' case
             while (x <= x1) list.add(matrix[x++][y]);
         }
-        return list;
+        */
+        return ret;
     }
     
     public static void main(String[] args) {
