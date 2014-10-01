f7d23ce362b4a2f1d9b16eb76faa3937a65ea8f7
Dongliang Yu <yudl.nju@gmail.com>
2014-10-01 19:18:00 +0800

    save space

--- src/solution/ValidSudoku.java
+++ src/solution/ValidSudoku.java
@@ -19,46 +19,46 @@ public class ValidSudoku {
         if (len1 != 9) return false;
         int len2 = board[0].length;
         if (len2 != 9) return false;
-        for (int i = 0; i < len1; i++)
+        for (int i = 0; i < 9; i++)
             if (!isValidRow(i, board)) return false;
-        for (int i = 0; i < len2; i++)
+        for (int i = 0; i < 9; i++)
             if (!isValidCol(i, board)) return false;
-        for (int i = 0; i < len1; i += 3)
-            for (int j = 0; j < len2; j += 3)
+        for (int i = 0; i < 9; i += 3)
+            for (int j = 0; j < 9; j += 3)
                 if (!isValidBox(i, j, board)) return false;
         return true;
     }
     
     private boolean isValidRow(int r, char[][] board) {
-        char[] row = new char[9];
+        boolean[] row = new boolean[9];
         for (int i = 0; i < 9; i++) {
             char cur = board[r][i];
             if (cur == '.') continue;
-            if (cur == row[cur - '1']) return false;
-            else row[cur - '1'] = cur;
+            if (row[cur - '1']) return false;
+            else row[cur - '1'] = true;
         }
         return true;
     }
     
     private boolean isValidCol(int c, char[][] board) {
-        char[] col = new char[9];
+    	boolean[] col = new boolean[9];
         for (int i = 0; i < 9; i++) {
             char cur = board[i][c];
             if (cur == '.') continue;
-            if (cur == col[cur - '1']) return false;
-            else col[cur - '1'] = cur;
+            if (col[cur - '1']) return false;
+            else col[cur - '1'] = true;
         }
         return true;
     }
     
     private boolean isValidBox(int r, int c, char[][] board) {
-        char[] box = new char[9];
+        boolean[] box = new boolean[9];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 char cur = board[r+i][c+j];
                 if (cur == '.') continue;
-                if (cur == box[cur - '1']) return false;
-                else box[cur - '1'] = cur;
+                if (box[cur - '1']) return false;
+                else box[cur - '1'] = true;
             }
         }
         return true;
