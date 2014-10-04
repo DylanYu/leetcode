a81d43a2006b8d81482c93a25b037f79674b512f
Dongliang Yu <yudl.nju@gmail.com>
2014-10-04 22:36:06 +0800

    another implementation

--- src/solution/MinimumWindowSubstring.java
+++ src/solution/MinimumWindowSubstring.java
@@ -24,6 +24,60 @@ import java.util.Map;
  *
  */
 public class MinimumWindowSubstring {
+	
+	private int count;
+    
+    public String minWindow(String S, String T) {
+        if (S == null || T == null) return null;
+        int lenS = S.length();
+        int lenT = T.length();
+        if (lenS == 0 || lenT == 0 || lenT > lenS) return "";
+        char[] s = S.toCharArray();
+        char[] t = T.toCharArray();
+        
+        Map<Character, Integer> limitMap = new HashMap<Character, Integer>();
+        for (int i = 0; i < t.length; i++)
+        	if (!limitMap.containsKey(t[i])) limitMap.put(t[i], 1);
+            else limitMap.put(t[i], limitMap.get(t[i])+1);
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        count = 0;
+        int minLen = Integer.MAX_VALUE;
+        String minStr = "";
+        
+        int start = 0;
+        int i = 0;
+        while (i < lenS) {
+            while (i < lenS && count < lenT) // increase i to collect enough characters in T
+                mapInc(map, limitMap, s[i++]);
+            if (i == lenS && count < lenT) break; // not possible
+            while (start < lenS && count == lenT) // increase start to eliminate redundant characters
+                mapDec(map, limitMap, s[start++]);
+            
+            if (i-start+1 < minLen) {
+                minLen = i-start+1;
+                minStr = S.substring(start-1, i);
+            }
+        }
+        return minStr;
+    }
+    
+    private void mapInc(Map<Character, Integer> map, Map<Character, Integer> limitMap, char key) {
+    	if (!limitMap.containsKey(key)) return;
+        if (!map.containsKey(key)) map.put(key, 1);
+        else map.put(key, map.get(key)+1);
+        if (map.get(key) <= limitMap.get(key)) // actions lead to exceeding limit does not count
+            count++;
+    }
+    
+    private void mapDec(Map<Character, Integer> map, Map<Character, Integer> limitMap, char key) {
+    	if (!limitMap.containsKey(key)) return;
+        if (!map.containsKey(key)) return;
+        if (map.get(key) <= limitMap.get(key))
+            count--;
+        map.put(key, map.get(key)-1);
+    }
+	
+	/**
     // idea from https://oj.leetcode.com/discuss/5469/is-the-length-of-t-considered-constant-or-m?show=5502#a5502
     public String minWindow(String S, String T) {
         //if (S.length() < T.length()) return ""; // handled by if(negativeCount>0)break in while loop
@@ -71,6 +125,7 @@ public class MinimumWindowSubstring {
         if (!map.containsKey(c)) map.put(c, -1);
         else map.put(c, map.get(c)-1);
     }
+    */
     
     public static void main(String[] args) {
         //String S = "ab";
