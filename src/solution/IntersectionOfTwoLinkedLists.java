b61d20794db6cb262ec2b813aeed4e2d3f2c159c
Dongliang Yu <yudl.nju@gmail.com>
2014-12-06 15:15:24 +0800

    add neat solution

--- src/solution/IntersectionOfTwoLinkedLists.java
+++ src/solution/IntersectionOfTwoLinkedLists.java
@@ -60,4 +60,23 @@ public class IntersectionOfTwoLinkedLists {
         }
         return p1;
     }
+    
+    /*
+     * interesting solution
+     * 
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        while (p1 != null && p2 != null && p1 != p2) {
+            p1 = p1.next;
+            p2 = p2.next;
+            // if intersection exists, return the intersection; or return null
+            if (p1 == p2) return p1;
+            if (p1 == null) p1 = headB;
+            if (p2 == null) p2 = headA;
+        }
+        return p1;
+    }
+    */
 }
