83f407445bc2f52141161f2d7cae3949557ecfed
Dongliang Yu <yudl.nju@gmail.com>
2014-12-11 14:43:02 +0800

    read n characters given read4

---
+++ src/solution/Reader4.java
@@ -0,0 +1,45 @@
+package solution;
+
+/**
+ * 
+ * @author Dongliang Yu
+ *
+ */
+public class Reader4 {
+    private String file;
+    private int offset;
+    
+    public Reader4(String s) {
+        file = s;
+        offset = 0;
+    }
+    
+    public int read4(char[] buf) {
+        int i = 0;
+        while (i < 4 && offset+i < file.length()) {
+            buf[i] = file.charAt(offset + i);
+            i++;
+        }
+        offset += i;
+        return i;
+    }
+    
+    public void show(char[] buf) {
+        for (char c : buf)
+            System.out.print(c + ",");
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        char[] buf = new char[4];
+        Reader4 reader = new Reader4("12345");
+        int n = reader.read4(buf);
+        System.out.println(n);
+        reader.show(buf);
+        
+        buf = new char[4];
+        n = reader.read4(buf);
+        System.out.println(n);
+        reader.show(buf);
+    }
+}
