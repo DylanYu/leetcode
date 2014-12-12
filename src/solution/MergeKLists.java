72dfc545680c7e8d2238ba9ccebf4d1119ac7986
Dongliang Yu <yudl.nju@gmail.com>
2014-12-12 10:54:20 +0800

    update and add notes

--- src/solution/MergeKLists.java
+++ src/solution/MergeKLists.java
@@ -24,9 +24,10 @@ public class MergeKLists {
         while (!heap.isEmpty()) {
             ListNode min = heap.poll();
             ListNode next = min.next;
-            min.next = null;
+            min.next = null; // not necessary but good practice
             p.next = min;
             p = min;
+            p.next = null;
             if (next != null) heap.add(next);
         }
         return dummy.next;
@@ -36,16 +37,16 @@ public class MergeKLists {
     // O(nklog(k)) time complexity, n is the size of every single original list 
     public ListNode mergeKLists(List<ListNode> lists) {
         if (lists == null || lists.size() == 0) return null;
-        int len = lists.size();
+        int size = lists.size();
         int step = 1;
-        while (step < len) {
-            int i = 0;
-            while (i < len) {
+        while (step < size) {
+            for (int i = 0; i < size; i += step*2) {
                 ListNode l1 = lists.get(i);
-                ListNode l2 = i+step < len ? lists.get(i+step) : null;
+                ListNode l2 = null;
+                if (i+step < size)
+                    l2 = lists.get(i+step);
                 lists.set(i, merge(l1, l2));
-                if (i+step < len) lists.set(i+step, null);
-                i += step*2;
+                //if (i+step < len) lists.set(i+step, null); // not necessary
             }
             step *= 2;
         }
@@ -55,23 +56,23 @@ public class MergeKLists {
     private ListNode merge(ListNode l1, ListNode l2) {
         if (l1 == null) return l2;
         if (l2 == null) return l1;
-        ListNode h1 = new ListNode(-1);
-        h1.next = l1;
-        ListNode p1 = h1;
-        while (l1 != null && l2 != null) {
-            if (l1.val > l2.val) {
+        ListNode dummy1 = new ListNode(-1);
+        dummy1.next = l1;
+        ListNode p1 = dummy1;
+        while (p1.next != null && l2 != null) {
+            if (p1.next.val > l2.val) {
                 ListNode tmp = l2;
                 l2 = l2.next;
+                tmp.next = p1.next;
                 p1.next = tmp;
-                tmp.next = l1;
                 p1 = p1.next;
             } else {
                 p1 = p1.next;
-                l1 = l1.next;
             }
         }
-        if (l1 == null) p1.next = l2;
-        return h1.next;
+        if (l2 != null) // do NOT forget this
+            p1.next = l2;
+        return dummy1.next;
     }
     */
 }
