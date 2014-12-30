d35daadcd011b9ea568920f00965f0d727c8f19f
Dongliang Yu <yudl.nju@gmail.com>
2014-12-30 20:16:56 +0800

    style update

--- src/solution/Atoi.java
+++ src/solution/Atoi.java
@@ -12,36 +12,37 @@ public class Atoi {
     // +, -, overflow , 0000000123, if encounter invalid character return last result
     public static int atoi(String str) {
         if (str == null) return 0; // error
-        str = str.trim();
+        str = str.trim(); // "    010" case
         char[] c = str.toCharArray();
         int len = c.length;
-        if (len == 0) return 0; // error
-        int sign = 0;
+        //if (len == 0) return 0; // error
         int i = 0;
-        if (c[0] == '+') { sign = 1; i++; }
-        else if (c[0] == '-') { sign = -1; i++; }
-        else sign = 1;
-        int num = 0;
+        int sign = 1;
+        if (c[0] == '-')
+            sign = -1;
+        if (c[0] == '-' || c[0] == '+')
+            i++;
+        int val = 0;
         while (i < len) {
             char ch = c[i];
             if (!(ch >= '0' && ch <= '9')) break; // error
             int digit = ch - '0';
             // deal with overflow
             if (sign == 1) {
-                if (num > MAX/10) return MAX;
-                num *= 10;
-                if (num > MAX-digit) return MAX;
-                num += digit;
+                if (val > MAX/10) return MAX;
+                val *= 10;
+                if (val > MAX-digit) return MAX;
+                val += digit;
             } else {
-                if (num > ((long) MAX + 1) / 10) return -MAX-1;
-                num *= 10;
-                if (num > (long) MAX + 1 - digit) return -MAX-1;
-                if (num-1+digit == MAX) return -MAX-1; //special case for Integer.MIN_VALUE
-                num += digit;
+                if (val > ((long) MAX + 1) / 10) return -MAX-1; // not necessary to use long(MAX)+1
+                val *= 10;
+                if (val-1 >= MAX-digit) return -MAX-1; // = is special case for Integer.MIN_VALUE (already overflow for = case)
+                //if (num-1+digit == MAX) return -MAX-1; //special case for Integer.MIN_VALUE
+                val += digit;
             }
             i++; //
         }
-        return sign * num;
+        return sign * val;
     }
 
     public static void main(String[] args) {
