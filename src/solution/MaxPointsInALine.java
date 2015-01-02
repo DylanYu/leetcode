4eb136d1c45fa728f227b16a56f0a94bf283f0fe
Dongliang Yu <yudl.nju@gmail.com>
2015-01-02 16:57:51 +0800

    update class Slope

--- src/solution/MaxPointsInALine.java
+++ src/solution/MaxPointsInALine.java
@@ -53,10 +53,22 @@ public class MaxPointsInALine {
     class Slope {
         long x;
         long y;
-        Slope(long a, long b) { x = a; y = b; }
+        public Slope(Point p, Point q) { 
+            x = (long) p.x - q.x; // deal with possible overflow
+            y = (long) p.y - q.y;
+            //if (x < 0) { x = -x; y = -y; } // not necessary
+            long gcd = gcd(x, y);
+            if (gcd != 0) {
+                x /= gcd;
+                y /= gcd;
+            }
+            if (x == 0 && y != 0) y = 1;
+            if (y == 0 && x != 0) x = 1;
+        }
         
         @Override
         public boolean equals(Object obj) {
+            if (obj == null) return false;
             if (obj == this) return true;
             if (!(obj instanceof Slope)) return false;
             Slope that = (Slope) obj;
@@ -66,7 +78,7 @@ public class MaxPointsInALine {
         @Override
         public int hashCode() {
             int ret = 17;
-            ret = ret * 31 + (int) (x ^ (x >>> 31));
+            ret = ret * 31 + (int) (x ^ (x >>> 31)); // unsigned shift, stuff with 0
             ret = ret * 31 + (int) (y ^ (y >>> 31));
             return ret;
         }
@@ -88,16 +100,7 @@ public class MaxPointsInALine {
                     coincide++;
                     continue;
                 }
-                long x = (long) p1.x - p2.x; // deal with possible overflow
-                long y = (long) p1.y - p2.y;
-                Slope slope;
-                if (x == 0) {
-                    // special vertical case.
-                    slope = new Slope(0, 1);
-                } else {
-                    long gcd = gcd(x, y);
-                    slope = new Slope(x/gcd, y/gcd);
-                }
+                Slope slope = new Slope(p1, p2);
                 if (!map.containsKey(slope)) map.put(slope, 0);
                 int n =  map.get(slope) + 1;
                 map.put(slope, n);
