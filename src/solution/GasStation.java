3f9eb5c848119ebb795138832a718cf99cce339a
Dongliang Yu <yudl.nju@gmail.com>
2015-01-05 20:14:42 +0800

    new explanation

--- src/solution/GasStation.java
+++ src/solution/GasStation.java
@@ -16,6 +16,31 @@ package solution;
  *
  */
 public class GasStation {
+    // start from i and end at j, try to modify i and j to satisfy requirements
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        if (gas == null || cost == null || gas.length != cost.length)
+            return -1;
+        int len = gas.length;
+        int i = 0;
+        int j = 0;
+        int tank = 0;
+        while (i < len) {
+            tank += (gas[j] - cost[j]);
+            j++;
+            if (j == len) j = 0;
+            if (tank < 0) {
+                while (i < len && tank < 0) {
+                    tank -= (gas[i] - cost[i]);
+                    i++;
+                }
+            } else {
+                if (i == j) return i;
+            }
+        }
+        return -1;
+    }
+    
+    /**
     // try to extend the tail and head until they meet
     public int canCompleteCircuit(int[] gas, int[] cost) {
         if (gas.length != cost.length) return -1;
@@ -39,6 +64,7 @@ public class GasStation {
         if (net < 0) return -1;
         else return j % len;// for j == len case, return 0
     }
+    */
     
     /**
     public int canCompleteCircuit(int[] gas, int[] cost) {
