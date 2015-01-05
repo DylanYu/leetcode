e08695cd96a11f62ad6c909d0a0ae6a1c7da9528
Dongliang Yu <yudl.nju@gmail.com>
2015-01-05 13:45:11 +0800

    add thinking about binary search return lo(hi)

--- src/solution/SearchForARange.java
+++ src/solution/SearchForARange.java
@@ -17,10 +17,9 @@ package solution;
  */
 public class SearchForARange {
     public int[] searchRange(int[] A, int target) {
-        int len = A.length;
         int[] ret = new int[2];
         int i = search(A, target);
-        if (/*i >= 0 && */i < len && A[i] == target) { // 
+        if (i < A.length && A[i] == target) { // i >= 0 is not necessary 
             int left = search(A, target-0.5);
             int right = search(A, target+0.5);
             ret[0] = left;
@@ -44,50 +43,44 @@ public class SearchForARange {
         return lo; // (possible) index of element which is >= target
     }
     
-    /*
+    /**
+     * same as the above one, thinking about the difference between "return lo" and "return hi"
+     * 
     public int[] searchRange(int[] A, int target) {
         int[] ret = new int[2];
-        int idx = binarySearch(A, target);
-        if (idx == -1) {
+        int i = search(A, target);
+        if (i >= 0 && A[i] == target) { // i < A.length is not necessary 
+            int left = search(A, target-0.5);
+            int right = search(A, target+0.5);
+            ret[0] = left+1;
+            ret[1] = right;
+        } else {
             ret[0] = -1;
             ret[1] = -1;
-        } else {
-            int l = searchInsertPosition(A, 0, idx, target-0.1);
-            int r = searchInsertPosition(A, idx, A.length-1, target+0.1);
-            ret[0] = l;
-            ret[1] = r-1;
         }
         return ret;
     }
     
-    private int binarySearch(int[] A, int target) {
+    private int search(int[] A, double target) {
         int lo = 0;
         int hi = A.length-1;
         while (lo <= hi) {
-            int mid = lo + (hi-lo)/2;
-            if (A[mid] < target) lo = mid+1;
-            else if (A[mid] > target) hi = mid-1;
-            else return mid;
+            int mid = lo + (hi-lo) /  2;
+            if (A[mid] == target) return mid;
+            else if (A[mid] < target) lo = mid+1;
+            else hi = mid-1;
         }
-        return -1;
-    }
-    
-    private int searchInsertPosition(int[] A, int lo, int hi, double target) {
-        while (lo <= hi) {
-            int mid = lo + (hi-lo)/2;
-            //if (A[mid] == target) return mid; // not possible
-            if (A[mid] < target) lo = mid+1;
-            else if (A[mid] > target) hi = mid-1;
-        }
-        return lo;
+        return hi; // (possible) index of element which is <= target
     }
     */
     
     public static void main(String[] args) {
-        int[] num = {5, 7, 7, 8, 8, 10};
-        int[] ret = new SearchForARange().searchRange(num, 7);
+        //int[] num = {5, 7, 7, 8, 8, 10};
+        //int[] ret = new SearchForARange().searchRange(num, 7);
         //int[] ret = new SearchForARange().searchRange(num, 4);
         //int[] ret = new SearchForARange().searchRange(num, 11);
+        int[] num = {2, 2};
+        int[] ret = new SearchForARange().searchRange(num, 2);
         System.out.println(String.format("[%d, %d]", ret[0], ret[1]));
     }
 }
