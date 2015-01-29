58f6087b5e539df42d11000339d43b4018eea3d5
Dongliang Yu <yudl.nju@gmail.com>
2015-01-29 22:12:25 +0800

    add edge case

--- src/solution/DivideTwoIntegers.java
+++ src/solution/DivideTwoIntegers.java
@@ -11,6 +11,8 @@ public class DivideTwoIntegers {
     public static int divide(int dividend, int divisor) {
         //if (dividend == 0) return 0; // handled by later logic
         //if (divisor == 0) throw new Exception();
+        if (dividend == Integer.MIN_VALUE && divisor == -1)
+            return Integer.MAX_VALUE;
         int sign = 0;
         if (dividend > 0 && divisor > 0 || dividend < 0 && divisor < 0) sign = 1;
         else sign = -1;
